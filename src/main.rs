847a90760748016dac9add3ce9cd8836ca387c55
Ethan <ethan.williamson.2006@gmail.com>
2023-07-04 16:24:51 +0100

    Implemented basic echo functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name = "echo")]
+#[command(author = "Ethan W <ethan.williamson.2006@gmail.com>")]
+#[command(version = "0.0")]
+#[command(about = "echoes", long_about = None)]
+
+struct Cli {
+    text: String,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    println!("{}", cli.text);
 }
